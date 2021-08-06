00815ea93b3d331aa920b5fbfa3c41a307a35d8f
kishantanwar <kishan.mca.du@gmail.com>
2021-08-06 12:14:07 +0530

    Main application update

--- src/main/java/com/epam/book/BookServiceApplication.java
+++ src/main/java/com/epam/book/BookServiceApplication.java
@@ -2,12 +2,19 @@ package com.epam.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+//@SpringBootApplication(scanBasePackages = "com.epam.book")
+//@EnableTransactionManagement
+//@EnableJpaRepositories(basePackages="com.epam.book.repositories")
 
-@SpringBootApplication(scanBasePackages = "com.epam.book")
-@EnableTransactionManagement
-@EnableJpaRepositories(basePackages="com.epam.book.repositories")
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+//@EnableEurekaClient
+@ComponentScan
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
